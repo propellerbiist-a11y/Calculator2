5e09c9cd0573156279e095e90bb30ee98c2d6752
Bhushan Vinayanand Mohod <propellerbiist@gmail.com>
2026-05-22 20:46:22 +0530

    Add basic calculator functions in script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+function appendValue(value) {
+
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+
+    document.getElementById("display").value = "";
+}
+
+function calculate() {
+
+    let expression = document.getElementById("display").value;
+
+    try {
+
+        let result = eval(expression);
+
+        document.getElementById("display").value = result;
+    }
+
+    catch {
+
+        document.getElementById("display").value = "Error";
+    }
+}
